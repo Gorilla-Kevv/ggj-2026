644c6ffb946914c53f957b2e5be1f61032a4db69
xiaoyukuki <xiaoyukuki@m.scnu.edu.cn>
2026-08-14 22:21:27 +0800

    飞行音效修改，删除不必要的特效

--- src/autoload/audio_manager.gd
+++ src/autoload/audio_manager.gd
@@ -229,9 +229,6 @@ func sfx_energy_out() -> void:
 	play_sfx(SFX_ENERGY_OUT, -4.0)
 
 func sfx_fly() -> void:
-	# 5% 概率触发飞行音效，避免频繁吹风时音效过于嘈杂
-	if randf() > 0.01:
-		return
 	play_sfx(SFX_FLY, -6.0)
 
 func sfx_find_player() -> void:
